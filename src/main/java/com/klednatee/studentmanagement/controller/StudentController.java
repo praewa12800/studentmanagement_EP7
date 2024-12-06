9133b165d0abc6304bc63729de7a8b4263edb40a
praewa ainsuwan <praewa12800@gmail.com>
2024-12-06 11:34:15 +0700

    first commit

---
+++ src/main/java/com/klednatee/studentmanagement/controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.klednatee.studentmanagement.controller;
+
+import com.klednatee.studentmanagement.dto.StudentDto;
+import com.klednatee.studentmanagement.service.StudentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
+    @GetMapping("/students")
+    public String ListStudent(Model model){
+        List<StudentDto> students = studentService.getAllStudents();
+        model.addAttribute("Students",students);
+        return "students";
+    }
+}
